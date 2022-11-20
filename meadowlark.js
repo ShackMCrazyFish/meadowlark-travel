1cb6dc7174cea26a738054d08cd29e88f9eea940
Виктор Дрыкин <drykin@qtim.ru>
2022-11-20 13:35:37 +0300

    New handlers module

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fortune = require('./lib/fortune');
 const ExpressHandlebars = require('express-handlebars')
+const handlers = require('./lib/handlers');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,24 +10,13 @@ app.engine('handlebars', ExpressHandlebars.engine({
 
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.get('/', handlers.home());
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() });
-});
+app.get('/about', handlers.about());
 
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
-});
+app.use(handlers.notFound());
 
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.status(500);
-    res.render('500');
-});
+app.use(handlers.serverError());
 
 app.listen(port, () => {
     console.log('Server started')
